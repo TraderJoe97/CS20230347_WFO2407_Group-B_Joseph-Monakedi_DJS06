44c3bb263a3801c54a147ff12eb6a1b1d398db91
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-10 15:37:16 +0200

    complete required exercises

--- scripts.js
+++ scripts.js
@@ -13,3 +13,35 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+// logs each name and each corresponding province to the console
+names.forEach((name, index) => {
+  console.log(`${name} (${provinces[index]})`)
+});
+
+// Creates a new array of provices in all uppercase
+const uppercaseProvinces = provinces.map(province => province.toUpperCase());
+console.log(uppercaseProvinces);
+
+// creates a new array containing the length of each name
+const nameLengths = names.map(name => name.length);
+console.log(nameLengths);
+
+// alphabetically sorts the provinces array
+const sortedProvinces = [...provinces].sort();
+console.log(sortedProvinces);
+
+// filters out provinces containing "Cape" logs the number of remaining provinces
+const filteredProvinces = provinces.filter(province => !province.includes("Cape"));
+console.log(filteredProvinces.length);
+
+// creates a boolean array using map and some to determine if a name contains the letter 'S'
+const containsS = names.map(name => name.split('').some(char => char === 'S'));
+console.log(containsS);
+
+// maps the names array into an object mapping names to their respective provinces
+const nameProvinceMap = names.reduce((map, name, index) => {
+  map[name] = provinces[index];
+  return map;
+}, {});
+console.log(nameProvinceMap);
